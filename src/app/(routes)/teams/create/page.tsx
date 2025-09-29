2d72846d40c0ce526cf601437a24511fb8c50bf1
VinChezz <vin4auzer@gmail.com>
2025-09-29 22:28:32 +0300

    feat: add middleware to teams/create page

--- src/app/(routes)/teams/create/page.tsx
+++ src/app/(routes)/teams/create/page.tsx
@@ -3,9 +3,10 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { Imprima } from "next/font/google";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 const imprima = Imprima({
   subsets: ["latin"],
@@ -13,9 +14,27 @@ const imprima = Imprima({
 });
 
 export default function CreateTeam() {
+  const { user, isLoading } = useKindeBrowserClient();
+  const [dbUser, setDbUser] = useState<any>(null);
   const [teamName, setTeamName] = useState("");
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      window.location.href =
+        "/api/auth/login?post_login_redirect_url=/teams/create";
+      return;
+    }
+
+    if (user) {
+      fetch("/api/auth/[kindeAuth]/kinde_callback")
+        .then((res) => res.json())
+        .then((data) => setDbUser(data));
+    }
+  }, [user, isLoading]);
+
+  if (!dbUser) return <p>Loading...</p>;
 
   const createNewTeam = async () => {
     try {
@@ -45,7 +64,10 @@ export default function CreateTeam() {
   };
   return (
     <div className="px-6 md:px-16 my-16">
-      <Image src={"/logo-black.png"} alt={"logo"} width={200} height={200} />
+      <div className="relative flex items-start justify-start">
+        <Image src={"/logo-1.png"} alt={"logo"} width={150} height={150} />
+        <div className="mt-1 ml-2 text-5xl text-black font-bold">Clario</div>
+      </div>
       <div className="flex flex-col items-center mt-8">
         <h2 className={`${imprima.className} text-[42px] py-2 font-extrabold`}>
           What should we call your team?
