d85851f07ed31615aeddf8de242a9aa525f9014b
VinChezz <vin4auzer@gmail.com>
2025-10-01 23:08:34 +0300

    feat: create FILE interface

---
+++ src/shared/types/file.interface.ts
@@ -0,0 +1,10 @@
+export interface FILE {
+  archive: boolean;
+  createdBy: string;
+  document: string;
+  fileName: string;
+  teamId: string;
+  whiteboard: string;
+  id: string;
+  _creationTime: number;
+}
