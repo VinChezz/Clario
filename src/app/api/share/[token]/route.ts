097bd6a17e7eadbad2376e95083f6f9381cf4796
VinChezz <vin4auzer@gmail.com>
2025-10-05 23:24:31 +0300

    feat: added asynchrony to the function

--- src/app/api/share/[token]/route.ts
+++ src/app/api/share/[token]/route.ts
@@ -3,10 +3,10 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(
   req: Request,
-  { params }: { params: { token: string } } // УБРАЛ async!
+  { params }: { params: Promise<{ token: string }> }
 ) {
   try {
-    const { token } = params; // ПРАВИЛЬНОЕ получение params
+    const { token } = await params;
 
     const file = await prisma.file.findFirst({
       where: { 
