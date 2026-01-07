13acac26aa17a2581924e43e659e1a2a2d005a32
VinChezz <vin4auzer@gmail.com>
2026-01-07 18:55:28 +0200

    style: increase the value of max-w-3xl to max-w-5xl

--- src/app/(routes)/settings/team/[teamId]/members/[memberId]/layout.tsx
+++ src/app/(routes)/settings/team/[teamId]/members/[memberId]/layout.tsx
@@ -4,6 +4,6 @@ export default function MemberLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto max-w-3xl px-6 py-8 space-y-8">{children}</div>
+    <div className="mx-auto max-w-5xl px-6 py-8 space-y-8">{children}</div>
   );
 }
