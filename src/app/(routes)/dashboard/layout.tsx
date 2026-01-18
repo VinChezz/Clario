aaa724f6e4230498c57e07109b60fd3bfde9f764
VinChezz <vin4auzer@gmail.com>
2026-01-18 14:05:33 +0200

    feat: add FavoritesProvider

--- src/app/(routes)/dashboard/layout.tsx
+++ src/app/(routes)/dashboard/layout.tsx
@@ -18,6 +18,7 @@ import { GithubProvider, useGithub } from "@/app/_context/GithubContext";
 import { CodeViewerModal } from "./_components/github-modal/_components/CodeViewer";
 import { SidebarProvider, useSidebar } from "@/app/_context/SidebarContext";
 import GradientLoader from "@/app/_loaders/GradientLoader";
+import { FavoritesProvider } from "@/app/_context/FavoritesContext";
 
 function GlobalCodeViewer() {
   const { codeViewerState, setCodeViewerState } = useGithub();
@@ -148,17 +149,18 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
     <TourProvider>
       <FileDataProvider>
         <GithubProvider>
-          <FileListContext.Provider value={{ fileList_, setFileList_ }}>
-            <div className="flex min-h-screen w-full bg-background overflow-hidden">
-              {isMobile && isSidebarOpen && (
-                <div
-                  className="fixed inset-0 bg-black/50 z-40 lg:hidden animate-in fade-in duration-300"
-                  onClick={closeSidebar}
-                />
-              )}
+          <FavoritesProvider>
+            <FileListContext.Provider value={{ fileList_, setFileList_ }}>
+              <div className="flex min-h-screen w-full bg-background overflow-hidden">
+                {isMobile && isSidebarOpen && (
+                  <div
+                    className="fixed inset-0 bg-black/50 z-40 lg:hidden animate-in fade-in duration-300"
+                    onClick={closeSidebar}
+                  />
+                )}
 
-              <div
-                className={`
+                <div
+                  className={`
                 fixed lg:static
                 top-0 left-0
                 h-screen
@@ -171,24 +173,25 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
                     : "-translate-x-full lg:translate-x-0 lg:shadow-none"
                 }
               `}
-              >
-                <div className="h-full bg-background border-r border-gray-200">
-                  <SideNav
-                    onCloseSidebar={closeSidebar}
-                    isMobileMenuOpen={isSidebarOpen}
-                  />
+                >
+                  <div className="h-full bg-background border-r border-gray-200">
+                    <SideNav
+                      onCloseSidebar={closeSidebar}
+                      isMobileMenuOpen={isSidebarOpen}
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div className="flex-1 flex flex-col h-screen overflow-hidden">
-                <div className="flex-1 overflow-y-auto">
-                  <GettingStartedTour />
-                  {children}
+                <div className="flex-1 flex flex-col h-screen overflow-hidden">
+                  <div className="flex-1 overflow-y-auto">
+                    <GettingStartedTour />
+                    {children}
+                  </div>
                 </div>
+                <GlobalCodeViewer />
               </div>
-              <GlobalCodeViewer />
-            </div>
-          </FileListContext.Provider>
+            </FileListContext.Provider>
+          </FavoritesProvider>
         </GithubProvider>
       </FileDataProvider>
     </TourProvider>
